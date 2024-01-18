16a32c61248c67ba164499f888d070b8b19db957
zsxlesz <136483423+zsxlesz@users.noreply.github.com>
2024-01-18 17:27:31 +0100

    initial

---
+++ javafxbea/src/main/java/com/example/javafxbea/Foprogram.java
@@ -0,0 +1,7 @@
+package com.example.javafxbea;
+
+public class Foprogram {
+    public static void main(String[] args) {
+        HelloApplication.main(args);
+    }
+}
---
+++ javafxbea/src/main/java/com/example/javafxbea/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.javafxbea;
+
+import javafx.fxml.FXML;
+
+import java.io.IOException;
+
+public class HelloController {
+
+    @FXML
+    private void OlvasMenuItem() throws IOException {
+
+
+    }
+
+}
